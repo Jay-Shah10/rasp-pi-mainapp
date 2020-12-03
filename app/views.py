a199949f109f25897b692dd2b8d6828ff18dd37c
Jay Shah <j24shah@gmail.com>
2020-12-02 20:38:12 -0500

    added in proper redirect
    updated index.html to show the form.
    updaed views.py to redirect to another flask app properly.

--- app/views.py
+++ app/views.py
@@ -6,9 +6,25 @@ import requests
 def index():
     return render_template('index.html')
 
-# @app.route("/zodiacapp")
-# def zodiacapp():
-#     try:
-#         return redirect(url_for('192.168.50.92:5001/'))
-#     except Exception:
-#         return Exception
+@app.route("/zodiacapp")
+def zodiacapp():
+    try:
+        # error code 302 will mean success for this connection.
+        return redirect('http://192.168.50.92:5001', code=302)
+    except Exception as e:
+        return e
+
+@app.route('/carlist')
+def carlist():
+    try:
+       return redirect('http://192.168.50.92:5002/carlist', code=302)
+    except Exception as e:
+        return e
+
+
+@app.route('/sensor')
+def sensor():
+   try:
+       return redirect('http://192.168.50.92:5000', code=302)
+   except Exception as e:
+       return e
